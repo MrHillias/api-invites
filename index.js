abbc3e14befbc3eff518c99416528ccdf608e0b9
Ilya <geil1997@mail.ru>
2024-10-04 13:15:32 +0300

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const sqlite3 = require("sqlite3").verbose();
+const { v4: uuidv4 } = require("uuid");
+
+const sequelize = require("./db_invites");
+const User = require("./models");
+
+const app = express();
+const port = 3000;
+
+// Middleware для обработки JSON
+app.use(bodyParser.json());
+
+// Разрешаем все источники
+app.use(cors());
+
+// Проверка соединения с БД
+sequelize
+  .authenticate()
+  .then(() => console.log("Соединение с базой данных установлено"))
+  .catch((err) => console.error("Невозможно подключиться к базе данных:", err));
+
+app.post("/invites/:chatId", async (req, res) => {
+  const chatId = req.params.chatId;
+  const uniqueCode = uuidv4();
+  const inviteLink = `https://t.me/drive/app?startapp=ref_${uniqueCode}`;
+  try {
+    await User.create({ chatId, code: uniqueCode, inviteLink });
+    res.status(201).json({ inviteLink });
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+});
+
+// Запуск сервера
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
