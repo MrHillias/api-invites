efd43cfba6be2a7ce5f94a411ab9181ede23f6cb
Ilya <geil1997@mail.ru>
2024-10-04 13:31:33 +0300

    init

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const sqlite3 = require("sqlite3").verbose();
 const { v4: uuidv4 } = require("uuid");
+const cors = require("cors");
 
 const sequelize = require("./db_invites");
 const User = require("./models");
